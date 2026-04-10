ba2f1525c59ce88dc539f34a0f9fea92bf23f8c6
fna106 <151074097+fna106@users.noreply.github.com>
2026-04-09 22:20:41 -0400

    Update postgres_database_configuration.py

--- postgres_database_configuration.py
+++ postgres_database_configuration.py
@@ -3,9 +3,9 @@ import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "136.114.68.152"),
-        database=os.getenv("DB_NAME", "freefoodmap"),
-        user=os.getenv("DB_USER", "postgres"),
+        host=f"/cloudsql/{os.getenv('INSTANCE_CONNECTION_NAME')}",
+        dbname=os.getenv("DB_NAME", "freefoodmap"),
+        user=os.getenv("DB_USER", "ffm_user"),
         password=os.getenv("DB_PASSWORD"),
         port=int(os.getenv("DB_PORT", 5432))
     )
