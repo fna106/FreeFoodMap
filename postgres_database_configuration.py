c36155e0f4d8a97e17b2b7a8f6c036fa1933b623
fna106 <151074097+fna106@users.noreply.github.com>
2026-04-09 20:23:22 -0400

    Updating the connection file to allow the app to connect to the database

--- postgres_database_configuration.py
+++ postgres_database_configuration.py
@@ -1,11 +1,11 @@
 import psycopg2
+import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password=******,
-        port=5432
+        host=os.getenv("DB_HOST", "136.114.68.152"),
+        database=os.getenv("DB_NAME", "freefoodmap"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD"),
+        port=int(os.getenv("DB_PORT", 5432))
     )
-
